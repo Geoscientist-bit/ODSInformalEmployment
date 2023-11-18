adc27602263d54a3010c2a6b47ab7eec3b7576de
Jhon Edison Muñoz <83843516+Geoscientist-bit@users.noreply.github.com>
2023-11-17 22:37:38 -0500

    avance proyecto

---
+++ src/main/java/org/ODSInformalEmployment/controller/MunicipalityController.java
@@ -0,0 +1,34 @@
+package org.ODSInformalEmployment.controller;
+
+import org.ODSInformalEmployment.model.MunicipalityData;
+import org.ODSInformalEmployment.repository.MunicipalityUsingFileRepositoryImpl;
+import org.ODSInformalEmployment.service.MunicipalityRecordService;
+import org.ODSInformalEmployment.service.MunicipalityRecordServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*
+
+ */
+@RestController
+@RequestMapping("/example/")
+@CrossOrigin(origins = "*")
+public class MunicipalityController {
+
+    MunicipalityRecordService municipalityRecordService = new MunicipalityRecordServiceImpl(new MunicipalityUsingFileRepositoryImpl());
+
+    @GetMapping
+    public List<MunicipalityData> municipalityDataList(){
+        System.out.println("Listando algo");
+
+        return municipalityRecordService.sortListPercentageInformality();
+
+    };
+
+
+
+
+
+}
---
+++ src/site/service/listar.js
@@ -0,0 +1,39 @@
+function listar(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'http://localhost:8080/grades/', true);
+
+    xhttp.onreadystatechange = function (){
+        if(xhttp.readyState === XMLHttpRequest.DONE){
+            var status = xhttp.status;
+            if (status === 0 || (status >= 200 && status < 400)){
+                var json = JSON.parse(xhttp.responseText);
+                createTable(json);
+            }else{
+                console.log("Error");
+            }
+        }
+    };
+    xhttp.send();
+}
+
+function createTable(json){
+    var tableBody = document.getElementById("table_body");
+
+    var child = tableBody.childElementCount;
+    for (let row = 1; row < child; row++){
+        tableBody.deleteRow(child-row);
+    }
+
+    //populate table
+    json.forEach((row) => {
+        var tr = document.createElement("tr");
+        Object.values(row).forEach((cell) => {
+
+            var td = document.createElement("td");
+            td.textContent = cell;
+            tr.appendChild(td);
+        });
+        tableBody.appendChild(tr);
+    });
+
+}
